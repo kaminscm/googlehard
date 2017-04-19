c93312049d4f808c2c008ba14eb7308a349755c0
Chris Kaminski <kaminski.m.chris@gmail.com>
2017-04-19 13:12:55 -0400

    done

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+var myImage = document.querySelector('img');
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/chrome-icon.png') {
+      myImage.setAttribute ('src','images/angry_face.png');
+    } else {
+      myImage.setAttribute ('src','images/chrome-icon.png');
+    }
+}
+
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
+
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
